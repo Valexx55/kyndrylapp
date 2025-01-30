c1ff082e9a446d6565c707d321e483d109105ecc
vale <valeriano@gmail.com>
2025-01-30 14:59:31 +0100

    busqueda alumno service 1/2

--- src/app/components/busqueda-alumno/busqueda-alumno.component.ts
+++ src/app/components/busqueda-alumno/busqueda-alumno.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-busqueda-alumno',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './busqueda-alumno.component.html',
   styleUrl: './busqueda-alumno.component.css'
 })
 export class BusquedaAlumnoComponent {
+
+  idbusqueda!:number;
+
+  buscarAlumnoPorId()
+ {
+  //TODO: usar el Alumno service para traerme el alumno del servidor  
+  console.log(`buscando alumno por id ${this.idbusqueda}` );
+ } 
 
 }
--- src/app/services/alumno.service.ts
+++ src/app/services/alumno.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Alumno } from '../model/alumno';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlumnoService {
 
-  constructor() { }
+  //aquí hacemos las conexiones por HTTP al servidor
+  constructor(private httpClient:HttpClient) { 
+    //this.httpClient.
+  }
+
+
+  buscarAlumnosPorId(id:number)
+ {
+    this.httpClient.get<Alumno>("http://localhost:9090/api/alumnos/"+id)
+ } 
 }
